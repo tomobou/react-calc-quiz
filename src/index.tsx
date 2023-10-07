a43454951bb85d594beca5264e59cc0a83f4b084
tomobounet <tomobounet@gmail.com>
2023-10-08 00:26:45 +0900

    add result

--- src/index.tsx
+++ src/index.tsx
@@ -11,12 +11,28 @@ class NumberSelector extends React.Component<NumberSelectorProps> {
     render() {
         let values = [...Array(11)].map((_, i) => i.toString());
         return (
-            <div className="number-selector-row">
-                {values.map((value) => {
-                    return (
-                        <button key={"number-selector-" + value} className="number-selector" onClick={() => this.props.onClick(value)}>{value}</button>
-                    )
-                })}
+            <div>
+                <div className="number-selector-row">
+                    {values.slice(0,1).map((value) => {
+                        return (
+                            <button key={"number-selector-" + value} className="number-selector" onClick={() => this.props.onClick(value)}>{value}</button>
+                        )
+                    })}
+                </div>
+                <div className="number-selector-row">
+                    {values.slice(1,6).map((value) => {
+                        return (
+                            <button key={"number-selector-" + value} className="number-selector" onClick={() => this.props.onClick(value)}>{value}</button>
+                        )
+                    })}
+                </div>
+                <div className="number-selector-row">
+                    {values.slice(6,11).map((value) => {
+                        return (
+                            <button key={"number-selector-" + value} className="number-selector" onClick={() => this.props.onClick(value)}>{value}</button>
+                        )
+                    })}
+                </div>
             </div>
         )
     }
@@ -33,28 +49,82 @@ interface QuestionerProps {
 class Questioner extends React.Component<QuestionerProps> {
     render() {
         if(this.props.currentQuiz){
-        return(
-        <div className="questioner">
-            <h3>
-                {this.props.whichQuiz+1}問目（ぜんぶで {this.props.quizs.length} 問）
-            </h3>
-            <div>
-                {this.props.currentQuiz && this.props.currentQuiz.q}
-                {this.props.wrongCount > 0 && <div className='shake'>{"".padStart(this.props.wrongCount,"×")}</div>}
+            return(
+            <div className="questioner">
+                <h3>
+                    {this.props.whichQuiz+1}問目（ぜんぶで {this.props.quizs.length} 問）
+                </h3>
+                <div className="question-content">
+                    {this.props.currentQuiz && this.props.currentQuiz.q}
+                    {this.props.wrongCount > 0 && <div className='shake'>{"".padStart(this.props.wrongCount,"×")}</div>}
+                </div>
             </div>
-        </div>
-        )
+            )
         }else{
             return(
-                <div className="questioner">
-                    <h3>もんだいをえらんでね！</h3>
-                    <button onClick={() => this.props.onClick("tasizan1")}>たしざん１</button>
-                </div>
+                <QuizSelector onClick={(value)=>this.props.onClick(value)}></QuizSelector>
                 )
         }
     }
 }
 
+interface QuizSelectorProps {
+    onClick: (value: string) => void
+}
+
+class QuizSelector extends React.Component<QuizSelectorProps> {
+    render() {
+        return(
+            <div className="questioner">
+                <h3>もんだいをえらんでね！</h3>
+                <div className="question-content">
+                <button onClick={() => this.props.onClick("tasizan1_3")}>れんしゅう</button>
+                </div>
+                <div className="question-content">
+                  <button onClick={() => this.props.onClick("tasizan1_ALL")}>たしざん１</button>
+                </div>
+            </div>
+            )
+    }
+}
+
+interface AnswerResultsViewProps {
+    totalLapTime: number,
+    results: AnswerResult[]
+}
+
+class AnswerResultsView extends React.Component<AnswerResultsViewProps> {
+    render() {
+        const results = this.props.results.sort((a,b)=> (a.wrongCount - b.wrongCount === 0)? (a.endTime!!-a.startTime!!)-(b.endTime!!-b.startTime!!): (a.wrongCount - b.wrongCount)).reverse();
+        
+        return(
+            <div>
+                <div>{this.props.totalLapTime/1000}秒でできたよ。</div>
+                <table className="answer-results-view">
+                    <thead>
+                        <th>問題</th>
+                        <th>間違えた数</th>
+                        <th>回答時間</th>
+                    </thead>
+                    <tbody>
+                    {results.map((result) => {
+                        const lapTime=(result.endTime!! - result.startTime!!);
+                        const wrongCountResult = result.wrongCount>0? "red":"";
+                        const lapTimeResult = lapTime<=1200? "" : lapTime<3000? "yellow" : "red";
+                            return (
+                                <tr>
+                                    <td>{result.quiz.q}</td>
+                                    <td className={['align-right',wrongCountResult].join(' ')}>{result.wrongCount}回</td>
+                                    <td className={['align-right',lapTimeResult].join(' ')}>{(lapTime/1000).toFixed(3)}秒</td>
+                                </tr>        
+                                )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+            )
+    }
+}
 
 
 function shuffle<T>(arr: Array<T>):Array<T> {
@@ -71,12 +141,20 @@ interface Quiz {
   q: string,
   a: number
 }
+interface AnswerResult {
+    quiz: Quiz,
+    wrongCount: number,
+    startTime?: number,
+    endTime?: number
+  }
+
 
 interface GameProps {
 
 }
 interface GameStates {
     quizs: Quiz[],
+    results: AnswerResult[]
     currentQuiz?: Quiz,
     whichQuiz: number,
     wrongCount: number,
@@ -84,23 +162,25 @@ interface GameStates {
     endTime?: number
 }
 
+function craeteQuizs(quizLevel: string): Array<Quiz>{
+    // 答えが10までのたしざん
+    let quizLevel1 = Array<Quiz>();
+    for(let a = 0; a < 10; a++){
+        for(let b = 0; b < 10; b++){
+            quizLevel1.push({q:`${a} + ${b} =`, a:(a+b)})
+        }
+    }
+    quizLevel1 = shuffle(quizLevel1.filter(quiz => quiz.a <= 10));
+    return quizLevel.endsWith("_3")? quizLevel1.slice(0,3):quizLevel1
+}
 
 class Game extends React.Component<GameProps, GameStates> {
     constructor(props: GameProps | Readonly<GameProps>) {
         super(props);
-
-        // 答えが10までのたしざん
-        let quizLevel1 = Array<Quiz>();
-        for(let a = 0; a < 10; a++){
-            for(let b = 0; b < 10; b++){
-                quizLevel1.push({q:`${a} + ${b} =`, a:(a+b)})
-            }
-        }
-        quizLevel1 = shuffle(quizLevel1.filter(quiz => quiz.a <= 10));
-
         //quizLevel1 = quizLevel1.slice(0,3);// テスト用に3問にする
         this.state = {
-            quizs: quizLevel1,
+            quizs: [],
+            results: [],
             currentQuiz: undefined,
             whichQuiz: -1,
             wrongCount: 0
@@ -109,17 +189,28 @@ class Game extends React.Component<GameProps, GameStates> {
     handleSelect(value: string) {
         if(this.state.currentQuiz){  
             if(this.state.currentQuiz.a === parseInt(value)) {
-                this.setState(state => ({wrongCount:0}));
+                const correctAnswerTime = Date.now();
+                const results = this.state.results;
+                const answerStartTime = (results.length>0)? results[results.length-1].endTime : this.state.startTime;
+                results.push({    quiz: this.state.currentQuiz,
+                    wrongCount: this.state.wrongCount,
+                    startTime: answerStartTime,
+                    endTime: correctAnswerTime
+                })
                 if(this.state.whichQuiz+1 === this.state.quizs.length){
                         this.setState(state => ({
                             currentQuiz: undefined,
+                            results: results,
                             whichQuiz: -1,
-                            endTime: Date.now()
+                            endTime: Date.now(),
+                            wrongCount: 0
                         }));
                     }else{
                         this.setState(state => ({
                             currentQuiz: this.state.quizs[this.state.whichQuiz+1],
-                            whichQuiz: this.state.whichQuiz + 1
+                            results: results,
+                            whichQuiz: this.state.whichQuiz + 1,
+                            wrongCount: 0
                         }));
                     }
             }else{
@@ -128,20 +219,22 @@ class Game extends React.Component<GameProps, GameStates> {
         }
     }
     handleStart(value: string){
+        const quizs = craeteQuizs(value)
         this.setState(state => ({
-            currentQuiz: this.state.quizs[0],
+            quizs: quizs,
+            results: [],
+            currentQuiz: quizs[0],
             whichQuiz: 0,
             startTime: Date.now(),
             endTime: undefined
         }));
     }
     render() {
-
         return (
             <div className="game">
                 <Questioner quizs={this.state.quizs} currentQuiz={this.state.currentQuiz} whichQuiz={this.state.whichQuiz} onClick={(value) => this.handleStart(value)} wrongCount={this.state.wrongCount}/>
-                <div>　{this.state.endTime && (this.state.endTime - this.state.startTime!!)/1000 + "秒でできたよ。"}</div>
                 <NumberSelector onClick={(value) => this.handleSelect(value)} />
+                {this.state.endTime && <AnswerResultsView totalLapTime={this.state.endTime!! - this.state.startTime!!} results={this.state.results} />}
             </div>
         )
     }
