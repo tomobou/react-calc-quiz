b4b52efae8fb3eadb2b0de0d4304d701175fdb31
tomobou <44117877+tomobou@users.noreply.github.com>
2026-03-24 11:08:27 +0900

    chore: format component files after conversion (#22)

--- src/components/AnswerResultsView.tsx
+++ src/components/AnswerResultsView.tsx
@@ -1,12 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { AnswerResult, getTotalLapTime } from "../domain/AnswerResult";
 
 interface AnswerResultsViewProps {
   results: AnswerResult[];
 }
-interface AnswerResultsViewStates {
-  active: string;
-}
+
 export function getGrade(results: AnswerResult[]): string {
   const wrongRate =
     (results.reduce(
@@ -27,8 +25,6 @@ export function getGrade(results: AnswerResult[]): string {
   } else {
     wrongGrade = 1;
   }
-  //console.log("wrongRate,Grade="+ wrongRate + "," + wrongGrade)
-
   const avgLapTime =
     results.reduce(
       (sum, result) => (sum += result.endTime! - result.startTime!),
@@ -46,79 +42,61 @@ export function getGrade(results: AnswerResult[]): string {
   } else {
     lapTimeGrade = 1;
   }
-  //console.log("avgLapTime,Grade="+ avgLapTime + "," + lapTimeGrade)
-
   return Math.min(wrongGrade, lapTimeGrade).toString();
 }
 
-export default class AnswerResultsView extends React.Component<
-  AnswerResultsViewProps,
-  AnswerResultsViewStates
-> {
-  constructor(
-    props: AnswerResultsViewProps | Readonly<AnswerResultsViewProps>,
-  ) {
-    super(props);
-    this.state = {
-      active: "",
-    };
-  }
+export default function AnswerResultsView({ results }: AnswerResultsViewProps) {
+  const [active, setActive] = useState<string>("");
 
-  hiddenResult() {
-    this.setState({ active: "hidden" });
-  }
+  const sortedResults = [...results]
+    .sort((a, b) =>
+      a.wrongCount - b.wrongCount === 0
+        ? (a.endTime ?? 0) -
+          (a.startTime ?? 0) -
+          ((b.endTime ?? 0) - (b.startTime ?? 0))
+        : a.wrongCount - b.wrongCount,
+    )
+    .reverse();
 
-  render() {
-    const results = [...this.props.results]
-      .sort((a, b) =>
-        a.wrongCount - b.wrongCount === 0
-          ? (a.endTime ?? 0) -
-            (a.startTime ?? 0) -
-            ((b.endTime ?? 0) - (b.startTime ?? 0))
-          : a.wrongCount - b.wrongCount,
-      )
-      .reverse();
+  const totalLapTime = getTotalLapTime(sortedResults);
+  const grade = getGrade(sortedResults);
 
-    const totalLapTime = getTotalLapTime(results);
-    const grade = getGrade(results);
-
-    return (
-      <div
-        className={["answer-result", this.state.active].join(" ")}
-        onClick={() => this.hiddenResult()}
-      >
-        <div>しゅうりょうー</div>
-        <div>{(totalLapTime / 1000).toFixed(3)}秒でできたよ。</div>
-        <img src={`/img/grade${grade}.png`} alt="grade" />
-        <table className="answer-results-view">
-          <thead>
-            <tr>
-              <th>問題</th>
-              <th>間違えた数</th>
-              <th>回答時間</th>
-            </tr>
-          </thead>
-          <tbody>
-            {results.map((result, index) => {
-              const lapTime = (result.endTime ?? 0) - (result.startTime ?? 0);
-              const wrongCountResult = result.wrongCount > 0 ? "red" : "";
-              const lapTimeResult =
-                lapTime <= 1200 ? "" : lapTime < 3000 ? "yellow" : "red";
-              return (
-                <tr key={"answer-results-row-" + index}>
-                  <td>{result.quiz.q}</td>
-                  <td className={["align-right", wrongCountResult].join(" ")}>
-                    {result.wrongCount}回
-                  </td>
-                  <td className={["align-right", lapTimeResult].join(" ")}>
-                    {(lapTime / 1000).toFixed(3)}秒
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+  return (
+    <div
+      className={"answer-result " + active}
+      onClick={() => setActive("hidden")}
+    >
+      <div>しゅうりょうー</div>
+      <div>{(totalLapTime / 1000).toFixed(3)}秒でできたよ。</div>
+      <img src={`/img/grade${grade}.png`} alt="grade" />
+      <table className="answer-results-view">
+        <thead>
+          <tr>
+            <th>問題</th>
+            <th>間違えた数</th>
+            <th>回答時間</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedResults.map((result, index) => {
+            const lapTime = (result.endTime ?? 0) - (result.startTime ?? 0);
+            const wrongCountResult = result.wrongCount > 0 ? "red" : "";
+            const lapTimeResult =
+              lapTime <= 1200 ? "" : lapTime < 3000 ? "yellow" : "red";
+            return (
+              <tr key={"answer-results-row-" + index}>
+                <td>{result.quiz.q}</td>
+                <td className={"align-right " + wrongCountResult}>
+                  {result.wrongCount}回
+                </td>
+                <td className={"align-right " + lapTimeResult}>
+                  {(lapTime / 1000).toFixed(3)}秒
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
--- src/components/QuizSelector.tsx
+++ src/components/QuizSelector.tsx
@@ -20,91 +20,82 @@ interface QuizSelectorProps {
   setQuizs: (quizs: Array<Quiz>) => void;
 }
 
-export default class QuizSelector extends React.Component<QuizSelectorProps> {
-  private quizButtons = Array<QuizButton>();
-  constructor(props: QuizSelectorProps | Readonly<QuizSelectorProps>) {
-    super(props);
-    this.quizButtons.push({
+export default function QuizSelector({ setQuizs }: QuizSelectorProps) {
+  const quizButtons: QuizButton[] = [
+    {
       name: "れんしゅう",
       remarks: "３もん",
       color: "#D8898A",
       quizBook: tasizan1,
       quizCount: 3,
-    });
-    this.quizButtons.push({
+    },
+    {
       name: "たしざん１",
       remarks: "ぜんぶ",
       color: "#D8898A",
       quizBook: tasizan1,
-    });
-    this.quizButtons.push({
+    },
+    {
       name: "ひきざん２",
       remarks: "５もん",
       color: "#84B7DC",
       quizBook: hikizan2,
       quizCount: 5,
-    });
-    this.quizButtons.push({
+    },
+    {
       name: "ひきざん２",
       remarks: "ぜんぶ",
       color: "#84B7DC",
       quizBook: hikizan2,
-    });
-    this.quizButtons.push({
+    },
+    {
       name: "たしざん３",
       remarks: "５もん",
       color: "#F8BA62",
       quizBook: tasizan3,
       quizCount: 5,
-    });
-    this.quizButtons.push({
+    },
+    {
       name: "たしざん３",
       remarks: "ぜんぶ",
       color: "#F8BA62",
       quizBook: tasizan3,
-    });
-    this.quizButtons.push({
+    },
+    {
       name: "ひきざん４",
       remarks: "５もん",
       color: "#86A884",
       quizBook: hikizan4,
       quizCount: 5,
-    });
-    this.quizButtons.push({
+    },
+    {
       name: "ひきざん４",
       remarks: "ぜんぶ",
       color: "#86A884",
       quizBook: hikizan4,
-    });
-  }
-  render() {
-    return (
-      <div className="questioner">
-        <h5>もんだいをえらんでね！</h5>
-        <div className="question-select">
-          {this.quizButtons.map((quizButton, index) => {
-            return (
-              <div
-                key={"question-select-item-" + index}
-                className="question-select-item"
-                style={{ backgroundColor: quizButton.color }}
-                onClick={() =>
-                  this.props.setQuizs(
-                    quizButton.quizBook.quizs(quizButton.quizCount),
-                  )
-                }
-              >
-                <div className="question-select-item-name">
-                  {quizButton.name}
-                </div>
-                <div className="question-select-item-remarks">
-                  {quizButton.remarks}
-                </div>
-              </div>
-            );
-          })}
-        </div>
+    },
+  ];
+
+  return (
+    <div className="questioner">
+      <h5>もんだいをえらんでね！</h5>
+      <div className="question-select">
+        {quizButtons.map((quizButton, index) => (
+          <div
+            key={"question-select-item-" + index}
+            className="question-select-item"
+            style={{ backgroundColor: quizButton.color }}
+            onClick={() =>
+              setQuizs(quizButton.quizBook.quizs(quizButton.quizCount))
+            }
+          >
+            <div className="question-select-item-name">{quizButton.name}</div>
+            <div className="question-select-item-remarks">
+              {quizButton.remarks}
+            </div>
+          </div>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
